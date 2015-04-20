782163b7203103bc26d748c2049d3655d42d50fe
Joe O'Conor <joconor@yhmg.com>
2015-04-19 20:47:04 -0400

    add github_handle and refactor student, add specs

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,17 @@ class Student < ActiveRecord::Base
   belongs_to :cohort
 
   def twitter_handle
-    self.twitter[/\/\w+\z/].gsub("/","") if self.twitter[/\/\w+\z/]
+    username(:twitter)
   end
+
+  def github_handle
+    username(:github)
+  end
+
+  private
+  # works for RESTful URLs such as http://www.twitter.com/boredelonmusk
+  def username(service)
+    send(service).to_s[/\w+$/] || ""
+  end
+
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+
   describe 'creating a new student' do
     context 'valid student' do
       it 'has a valid factory' do
@@ -17,6 +18,39 @@ RSpec.describe Student, type: :model do
         student = create(:student, :cohort => cohort)
         expect(student.cohort).to eq(cohort)
       end
+    end
+  end
+
+  describe 'username extracting' do
+    let(:student) { create(:student, twitter: twitter_link, github: github_link) }
+
+    context 'when the user has typical service link entries' do 
+      let(:github_link) { "http://github.com/jnoconor" }
+      let(:twitter_link) { "http://www.twitter.com/boredelonmusk" }
+
+      it "#twitter_handle correctly extracts the user's Twitter username" do
+        expect(student.twitter_handle).to eq('boredelonmusk')
+      end
+
+      it "#github_handle correctly extracts the user's Github username" do
+          expect(student.github_handle).to eq('jnoconor')
+      end
+    end
+
+    context 'when the user has no link entries' do 
+      let(:github_link) { nil }
+      let(:twitter_link) { nil }
+
+      it "#twitter_handle returns an empty string" do
+        expect{ student.twitter_handle }.to_not raise_error
+        expect(student.twitter_handle).to eq('')
+      end
+
+      it "#github_handle returns an empty string" do
+        expect{ student.github_handle }.to_not raise_error
+        expect(student.github_handle).to eq('')
+      end
+
     end
   end
 end
